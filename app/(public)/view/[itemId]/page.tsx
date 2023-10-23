dba54e8e578b7d5e49ecb196533baa924267314a
Davy-71993 <davy.kyute@gmail.com>
2023-10-23 23:18:46 +0300

    first commit

--- app/(public)/view/[itemId]/page.tsx
+++ app/(public)/view/[itemId]/page.tsx
@@ -1,5 +1,7 @@
+import ProductCard from "@/components/parts/product-card";
+import { Button } from "@/components/ui/button";
 import { Products } from "@/lib/dami-api";
-import { Product } from "@/lib/types";
+import { MapPin, MessageCircle, PhoneCall, Video } from "lucide-react";
 import Image from "next/image";
 
 type Props = {
@@ -27,14 +29,45 @@ export default function ItemPage({ params }: Props) {
           Market price  $ 
           <span className="ml-10">{ product?.price - 10} - { product?.price + 10 }</span>
         </p>
-        <div className="bg-primary-foreground p-5 flex">
-          <Image src='/images/gfgh.png' alt="Seller Image" width={100} height={10}  />
-          <div className="pl-3">
-            <h4 className="text-lg">Top Phone Seller</h4>
+        <div className="bg-primary-foreground p-5 flex flex-col sm:flex-row">
+          <Image src='/images/gfgh.png' alt="Seller Image" width={100} height={10}
+            className="w-full h-auto max-w-[200px] max-h-[200px] self-center"  />
+          <div className="pl-3 w-full px-10">
+            <h4 className="text-lg font-bold text-center">Top Phone Seller</h4>
+            <p className="mt-5">Best deals in smart phones, laotops and computer accessories</p>
+            <p className="my-5 flex space-x-5"><span><PhoneCall/></span><span>+256777921033</span></p>
+            <p className="my-5 flex space-x-5"><span><MessageCircle/></span><span>+256777921033</span></p>
+            <div className="flex space-x-5">
+              <Button variant={'outline'} className="rounded flex space-x-5 line-clamp-1"><Video /><span className="line-clamp-1">Call now!</span></Button>
+              <Button variant={'outline'} className="rounded flex space-x-5 line-clamp-1"><MapPin /><span className="line-clamp-1">Find on map!</span></Button>
+            </div>
           </div>
         </div>
       </div>
-      <div className="col-span-6 mt-5 border p-5 w-full lg:col-span-1"></div>
+      <div className="col-span-6 mt-5 border-r p-5 w-full lg:col-span-1"></div>
+      <div className="col-span-6 py-5 border-t border-b min-h-[20vh] mt-5 rounded">
+        <h2 className="text-2xl">More about this item</h2>
+        <div className="flex flex-wrap py-5">
+          {
+            [2,3,4,5,6,7,8,9,10].map((w,i) => (
+              <div className="p-3 m-2" key={i}>
+                <p className="text-sm">Condition</p>
+                <p className="font-bold">Pre-owned</p>
+              </div>
+            ))
+          }
+        </div>
+      </div>
+      <div className="col-span-6 mt-5">
+      <h2 className="text-2xl">More of similar items</h2>
+        <div className="grid gap-10 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mt-10">
+          {
+            Products.map((it, i) =>(
+              <ProductCard item={it} key={i} />
+            ))
+          }
+        </div>
+      </div>
     </div>
   )
 }
