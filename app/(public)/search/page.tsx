0aa1fbda45989dae683a7061dfd6e01b0f7a6cae
Davy-71993 <davy.kyute@gmail.com>
2023-10-23 10:01:16 +0300

    first commit

--- app/(public)/search/page.tsx
+++ app/(public)/search/page.tsx
@@ -1,9 +1,17 @@
+import ProductCard from '@/components/parts/product-card'
+import { Products } from '@/lib/dami-api'
 import React from 'react'
 
 type Props = {}
 
 export default function page({}: Props) {
   return (
-    <div>page</div>
+    <div className="grid gap-10 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mt-10">
+        {
+          Products.map((it, i) =>(
+            <ProductCard item={it} key={i} />
+          ))
+        }
+      </div>
   )
 }
--- app/(public)/view/[itemId]/page.tsx
+++ app/(public)/view/[itemId]/page.tsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import { Products } from "@/lib/dami-api";
+import { Product } from "@/lib/types";
+import Image from "next/image";
 
-type Props = {}
+type Props = {
+  params: any
+}
 
-export default function ItemPage({}: Props) {
+export default function ItemPage({ params }: Props) {
+  const products = Products
+  const { itemId } = params
+  const product = products[0]
   return (
-    <div>ItemPage</div>
+    <div className='grid grid-cols-6'>
+      <div className="col-span-6 sm:col-span-2">
+        <Image 
+          src={product?.image} 
+          height={100} width={1000} 
+          className="w-full h-auto"
+          alt="Product Image" />
+      </div>
+      <div className="col-span-6 sm:col-span-4 lg:col-span-3 sm:pl-10 mt-5 sm:mt-0">
+        <h4 className="text-2xl font-bold">{ product?.title }</h4>
+        <p className="text-base">{ product?.description }</p>
+        <h4 className="text-4xl font-bold my-3">$ { product?.price }</h4>
+        <p className="border p-3 text-center my-5">
+          Market price  $ 
+          <span className="ml-10">{ product?.price - 10} - { product?.price + 10 }</span>
+        </p>
+        <div className="bg-primary-foreground p-5 flex">
+          <Image src='/images/gfgh.png' alt="Seller Image" width={100} height={10}  />
+          <div className="pl-3">
+            <h4 className="text-lg">Top Phone Seller</h4>
+          </div>
+        </div>
+      </div>
+      <div className="col-span-6 mt-5 border p-5 w-full lg:col-span-1"></div>
+    </div>
   )
 }
